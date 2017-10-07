f092e67377ee3d4aad3f58fcb0d0993d4c78ea19
Liam Kelly <32558851+likel@users.noreply.github.com>
2017-10-07 13:22:01 +1000

    Fix whacky Atom line spacing

--- autoload.php
+++ autoload.php
@@ -19,24 +19,24 @@
  * @return void
  */
 spl_autoload_register(function ($class_name) {
-    // Change these depending on the project
-    $project_prefix = 'Fizz\\Buzz\\';
-    $models_dir = __DIR__ . '/models/';
+  // Change these depending on the project
+  $project_prefix = 'Fizz\\Buzz\\';
+  $models_dir = __DIR__ . '/models/';
 
-    // Helper variables used in the autoloader
-    $project_prefix_length = strlen($project_prefix);
-    $relative_class = substr($class_name, $project_prefix_length);
+  // Helper variables used in the autoloader
+  $project_prefix_length = strlen($project_prefix);
+  $relative_class = substr($class_name, $project_prefix_length);
 
-    // Return if the requested class does not include the prefix
-    if (strncmp($project_prefix, $class_name, $project_prefix_length) !== 0) {
-      return;
-    }
+  // Return if the requested class does not include the prefix
+  if (strncmp($project_prefix, $class_name, $project_prefix_length) !== 0) {
+    return;
+  }
 
-    // Replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the class name and append with .php
-    $file = $models_dir . str_replace('\\', '/', $relative_class) . '.php';
+  // Replace the namespace prefix with the base directory, replace namespace
+  // separators with directory separators in the class name and append with .php
+  $file = $models_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-    if (file_exists($file)) {
-      require_once($file);
-    }
+  if (file_exists($file)) {
+    require_once($file);
+  }
 });
