d4f315c15d61df995b4dac73c8bf5f3991da5f7d
Liam Kelly <32558851+likel@users.noreply.github.com>
2017-10-07 11:13:01 +1000

    Object class initial creation
    Removed Something class, added Object class and edited autoload

--- autoload.php
+++ autoload.php
@@ -10,21 +10,26 @@
  * This will attempt to load /path/to/project/models/Class/Model.php
  *
  * @author Liam Kelly (likel)
+ * @created 07/10/2017
+ * @version 1.0.0
+ */
+
+/**
  * @param string $class_name The requested class name.
  * @return void
  */
 spl_autoload_register(function ($class_name) {
-	// Change these depending on the project
+    // Change these depending on the project
     $project_prefix = 'Fizz\\Buzz\\';
-	$models_dir = __DIR__ . '/models/';
-	
-	// Helper variables used in the autoloader
+    $models_dir = __DIR__ . '/models/';
+
+    // Helper variables used in the autoloader
     $project_prefix_length = strlen($project_prefix);
     $relative_class = substr($class_name, $project_prefix_length);
-	
-	// Return if the requested class does not include the prefix
+
+    // Return if the requested class does not include the prefix
     if (strncmp($project_prefix, $class_name, $project_prefix_length) !== 0) {
-        return;
+      return;
     }
 
     // Replace the namespace prefix with the base directory, replace namespace
@@ -32,6 +37,6 @@ spl_autoload_register(function ($class_name) {
     $file = $models_dir . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file)) {
-        require_once($file);
+      require_once($file);
     }
 });
